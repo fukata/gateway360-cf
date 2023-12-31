ebeecc5d9cbe44b10ca4a393ad3ed82f831f72da
fukata <tatsuya.fukata@gmail.com>
2023-12-31 13:44:38 +0900

    add husky, biome

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
+import { serveStatic } from "hono/cloudflare-workers";
+
 const app = new Hono()
 
+app.get('/static/*', serveStatic({ root: './' }))
 app.get('/', (c) => c.text('Hello World!'))
 
 export default app
